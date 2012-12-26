380ea0afc8b487940ff3c03760d6ffd48d9e1ca8
Peter Szabo <pts@fazekas.hu>
2012-12-26 15:41:15 +0100

    improved rebuild_db.py

--- rebuild_db.py
+++ rebuild_db.py
@@ -1,20 +1,18 @@
 #! /bin/sh --
-""":" # Rebuilds (parts of) metadata.db from the metadata.opf files.
+""":" Creates missing metadata.opf files from metadata.db
 
 exec calibre-debug -e "$0" ${1+"$@"}
 
 Don't run this script while somebody else (e.g. Calibre) is modifying the
-library. If you do anyway, you may lose some data. To be safe, exit from
-Calibre while this script is running.  !!
+library. If you do anyway, you may lose some data.
 """
 
-# by pts@fazekas.hu at Wed Dec 26 12:54:02 CET 2012
+# by pts@fazekas.hu at Sun Dec 23 11:14:15 CET 2012
 __author__ = 'pts@fazekas.hu (Peter Szabo)'
 
 import cStringIO
 import os
 import os.path
-import sqlite3
 import sys
 
 import calibre
@@ -37,7 +35,7 @@ def decode_unicode(data):
 
 
 def usage(argv0):
-  return ('Rebuilds (parts of) metadata.db from the metadata.opf files.\n'
+  return ('Creates missing metadata.opf files from metadata.db\n'
           'Usage: %s [<calibre-library-dir>]' % argv0)
 
 
@@ -49,27 +47,39 @@ def main(argv):
     print >>sys.stderr, usage()
     sys.exit(1)
   dbdir = argv[1].rstrip(os.sep) if len(argv) > 1 else '.'
-  if os.path.isfile(dbdir):
-    dbname = dbdir
-  else:
-    dbname = os.path.join(dbdir, 'metadata.db')
-  del dbdir
+  dbname = os.path.join(dbdir, 'metadata.db')
   dbpath = os.path.abspath(dbname)
-  print >>sys.stderr, 'info: Rebuilding Calibre database: ' + dbpath
+  print >>sys.stderr, 'info: Reading Calibre database: ' + dbpath
   if not os.path.isfile(dbname):
     raise AssertionError('Calibre database missing: %s' % dbpath)
-  conn = sqlite3.connect(dbname, check_same_thread=0,
-                         isolation_level='EXCLUSIVE')
-  conn.text_factory = str
-  print conn.isolation_level
-  c = conn.cursor()
-  print dir(c)
-  print dir(conn)
-  #c.execute('BEGIN')
-  c.execute('BEGIN EXCLUSIVE')  # Locks the file immediately.
-  for row in c.execute('SELECT * FROM sqlite_master'):
-    print row
-  import time; time.sleep(42)
+  dbdirpath = os.path.dirname(dbpath)
+  # Reads the whole metadata.db to memory.
+  db = database2.LibraryDatabase2(dbdirpath)
+  fm = db.FIELD_MAP
+  fm_path = fm['path']
+  # TODO(pts): Do we have to replace / with os.sep on Windows?
+  book_path_items = [(i, decode_unicode(row[fm_path]))
+     for i, row in enumerate(db.data._data) if row is not None]
+  print >>sys.stderr, 'info: Found %d book%s in metadata.db.' % (
+      len(book_path_items), 's' * (len(book_path_items) != 1))
+  book_opf_items = []
+  for i, book_path in book_path_items:
+    opf_path = os.path.join(dbdirpath, book_path, 'metadata.opf')
+    if not os.path.exists(opf_path):
+      book_opf_items.append((i, opf_path))
+  print >>sys.stderr, 'info: Found %d book%s with missing metadata.opf.' % (
+      len(book_opf_items), 's' * (len(book_opf_items) != 1))
+  # TODO(pts): Find directories not corresponding to any books in the database.
+  for i, opf_path in book_opf_items:
+    mi = db.get_metadata(i, index_is_id=True)
+    print >>sys.stderr, 'info: Creating metadata.opf: %s' % opf_path
+    opf_creator = opf2.OPFCreator(os.path.dirname(opf_path), mi)
+    sio = cStringIO.StringIO()
+    opf_creator.render(sio)
+    with open(opf_path, 'wb') as f:
+      f.write(sio.getvalue())
+  print >>sys.stderr, 'info: metadata.opf creation done.'
+
 
 if __name__ == '__main__':
   # SUXX: Original, byte argv not available.
